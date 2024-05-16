eae2b6f7bbc2ec1a68cd735b93bafeaa83ec3fa9
Patserbak <36964214+Patserbak@users.noreply.github.com>
2024-05-16 21:02:10 +0200

    Update lambda.js

--- terraform_lambda_api/src/lambda.js
+++ terraform_lambda_api/src/lambda.js
@@ -1,8 +1,6 @@
-
 import { SESClient } from "@aws-sdk/client-ses";
 const sesClient = new SESClient({ region: "eu-central-1" });
 import { SendEmailCommand } from "@aws-sdk/client-ses";
-import crypto from "crypto";
 
 var response = {
   "statusCode": 200,
@@ -12,23 +10,12 @@ var response = {
   "body": "{ \"result\": \"Send email\"\n}"
 }
 
-const pKey = crypto.createPublicKey({
-  key: Buffer.from(process.env.PUBLIC_KEY, "base64").toString(),
-  format: "pem",
-  type: "pkcs1"
-});
-
 export const handler = async (event) => {
 
   if (event.body?.length > 550) throw new Error("bad request");
 
   const e = JSON.parse(event.body);
-  if (!e.name || !e.email || !e.message || !e.contactEmail || !e.signatureContactEmail || !e.url) throw new Error("bad request");
-
-  const algorithm = "SHA256";
-  const eBuf = Buffer.from(e.contactEmail+e.url);
-  const sBuf = Buffer.from(e.signatureContactEmail, "base64");
-  if (!crypto.verify(algorithm, eBuf, pKey, sBuf)) throw new Error("bad request");
+  if ( !e.contactEmail || !e.subject || !e.sourceEmail) throw new Error("bad request");
 
   const params = new SendEmailCommand({
     Destination: {
@@ -40,17 +27,17 @@ export const handler = async (event) => {
     },
     Message: {
       Body: {
-        Text: {
+        Html: {
+          Data: e.body,
           Charset: "UTF-8",
-          Data: `Hello,\n\nPlease find here the details of the Contact Form sent from your website:\n\n - Name: ${e.name}\n - Email: ${e.email}\n\n\n----- Start Message -----\n\n${e.message}\n\n\n\n ------ End Message ------\n\n Kind Regards,\n\nadfreesites.com`,
-        },
+        }
       },
       Subject: {
         Charset: "UTF-8",
-        Data: `CONTACT FROM ${e.url}`,
+        Data: e.subject,
       },
     },
-    Source: process.env.FROM_EMAIL,
+    Source: e.sourceEmail,
     ReplyToAddresses: [
     ],
   });
